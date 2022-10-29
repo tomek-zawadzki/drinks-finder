8a73fc1e663a9bf8270ea3c3f153cda6b89ef028
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-10-29 17:17:21 +0200

    change for loop to forEach method and implement function to search drinks

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const drinks = document.querySelectorAll(".ul__drink");
 const inputText = input.value;
 console.log(drinks);
 
-input.addEventListener("keyup", () => {
-  for (let i = 0; i < drinks.length; i++) {
-    const drink = drinks[i].textContent;
-    console.log(typeof drink);
-    console.log(input.value);
-    if (input.value === drink) console.log("ok");
-    else console.log('nie ok');
-  }
-});
+const finder = function () {
+  drinks.forEach((el) => {
+    const task = el.textContent;
+
+    if (task.toLowerCase().indexOf(input.value.toLowerCase()) !== -1) {
+      el.style.display = "block";
+    } else {
+      el.style.display = "none";
+    }
+  });
+  //   for (let i = 0; i < drinks.length; i++) {
+  //     const drink = drinks[i].textContent;
+  //     // console.log(drink);
+  //     console.log(input.value);
+  //     if (input.value.toLowerCase() == drink.toLowerCase()) {
+  //       console.log("✅✅✅✅✅ ok");
+  //     } else {
+  //       console.log("🔴🔴🔴🔴🔴 nie ok");
+  //       drink;
+  //     }
+  //   }
+};
+
+input.addEventListener("keyup", finder);
