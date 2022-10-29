e31075e584f9a9ff6925cf5d8a6e85b065dc9ac6
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-10-29 11:35:02 +0200

    handle evntListener and use for loop to get string from node list

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+const input = document.querySelector(".input");
+const drinks = document.querySelectorAll(".ul__drink");
+const inputText = input.value;
+console.log(drinks);
+
+input.addEventListener("keyup", () => {
+  for (let i = 0; i < drinks.length; i++) {
+    const drink = drinks[i].textContent;
+    console.log(typeof drink);
+    console.log(input.value);
+    if (input.value === drink) console.log("ok");
+    else console.log('nie ok');
+  }
+});
